1227a8b38e35ae6ead3ed2faf6f82e5108f98909
HughG-50 <hugh.greethead@gmail.com>
2020-09-13 12:24:19 +1000

    Basic db seed added for testing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,43 +1,45 @@
 class Listing < ApplicationRecord
 
-def self.populate_ryde_listings
-    # Remove what was in database previously
-    Listing.destroy_all
+    def self.populate_ryde_listings
+        # Remove what was in database previously
+        Listing.destroy_all
 
-    endpoint = ""
-    request = HTTParty.post()
-    json = JSON.parse(request.body)
-
-    if !json.nil?
-        json.map do |json_item|
-            if ((json_item["listing"]["headline"].downcase || json_item["listing"]["summaryDescription"].downcase).include? ("duplex"||"development"))
-                listing = Listing.new
-                listing.domain_listing_id = json_item["listing"]["id"]
-                listing.address = json_item["listing"]["propertyDetails"]["displayableAddress"]
-                listing.land_area = json_item["listing"]["propertyDetails"]["landArea"]
-                listing.headline_desc = json_item["listing"]["headline"]
-                listing.summary_desc = json_item["listing"]["summaryDescription"]
-                listing.display_price = json_item["listing"]["priceDetails"]["displayPrice"]
-                listing.agency_name = json_item["listing"]["advertiser"]["name"]
-                listing.agent = json_item["listing"]["advertiser"]["contacts"][0]["name"]
-                listing.save
-            end
-        end
-    else 
-        puts "Error with search API call"
-    end
-
-    listings = Listing.all
-    for listing in listings 
-        i_listing_endpoint = ".../#{listing.domain_listing_id}"
-        request = HTTParty.get(i_listing_endpoint)
+        endpoint = ""
+        request = HTTParty.post()
         json = JSON.parse(request.body)
 
-        listing.listing_url = json["seoUrl"]
-        listing.save
-    end
-end
+        if !json.nil?
+            json.map do |json_item|
+                if ((json_item["listing"]["headline"].downcase || json_item["listing"]["summaryDescription"].downcase).include? ("duplex"||"development"))
+                    listing = Listing.new
+                    listing.domain_listing_id = json_item["listing"]["id"]
+                    listing.address = json_item["listing"]["propertyDetails"]["displayableAddress"]
+                    listing.land_area = json_item["listing"]["propertyDetails"]["landArea"]
+                    listing.headline_desc = json_item["listing"]["headline"]
+                    listing.summary_desc = json_item["listing"]["summaryDescription"]
+                    listing.display_price = json_item["listing"]["priceDetails"]["displayPrice"]
+                    listing.agency_name = json_item["listing"]["advertiser"]["name"]
+                    listing.agent = json_item["listing"]["advertiser"]["contacts"][0]["name"]
+                    listing.save
+                end
+            end
+        else 
+            puts "Error with search API call"
+        end
 
-def self.populate_strathfield_listings
-    # same but without the condition string checking for "duplex" and "development"
+        listings = Listing.all
+        for listing in listings 
+            i_listing_endpoint = ".../#{listing.domain_listing_id}"
+            request = HTTParty.get(i_listing_endpoint)
+            json = JSON.parse(request.body)
+
+            listing.listing_url = json["seoUrl"]
+            listing.save
+        end
+    end
+
+    def self.populate_strathfield_listings
+        # same but without the condition string checking for "duplex" and "development"
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Sample seed data for testing purposes only
+
+listings = Listing.create([
+    {   
+        domain_listing_id: 2016446393, 
+        address: "20 Pellisier Road, Putney", 
+        land_area: 663, 
+        headline_desc: "Prized Corner on 663sqm | Prestigious Locale with Water Views | Rare & Ready", 
+        summary_desc: "<b></b><br />Perched proudly on a privileged 663qm corner parcel only a gentle stroll to Putneys amazing waterfront foreshores, on offer is an exciting opportunity to capitalize in a premium blue-chip location.\r\n\r\nReady to be transformed into the sophi...",
+        listing_url: "https://www.domain.com.au/130-abuklea-road-eastwood-nsw-2122-2016276180",
+        display_price: "AUCTION  Robert Bagala 0418 220 882",
+        agency_name: "First National Real Estate Hunters Hill, Gladesville & Ryde",
+        agent: "Robert Bagala",
+    }
+])
