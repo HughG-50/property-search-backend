2e9fb7cf04f4d4d6586ec9e00bea2be5a5d0e082
HughG-50 <hugh.greethead@gmail.com>
2020-09-17 22:21:16 +1000

    Changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# blah blah blah
+
 ActiveRecord::Schema.define(version: 2020_08_23_114947) do
 
   # These are extensions that must be enabled in order to support this database
