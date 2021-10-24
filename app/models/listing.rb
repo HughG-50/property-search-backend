7b1d4eb356c79c84f8f9db28eda5894a69331ca2
HughG-50 <hugh.greethead@gmail.com>
2021-10-24 22:27:49 +1100

    Update page size

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -852,7 +852,7 @@ class Listing < ApplicationRecord
               minCarspaces: 0,
               minLandArea: 500,
               maxPrice: 600000,
-              pageSize: 50,
+              pageSize: 100,
               sort: {
                 sortKey: "DateUpdated",
                 direction: "Ascending",
